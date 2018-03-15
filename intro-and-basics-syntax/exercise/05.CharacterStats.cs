aa6223d323e22c851297adef9ae00fb135955f52
AntoniaShalamanova <antonia.shalamanova@gmail.com>
2018-03-15 14:34:50 +0200

    Make changes on variables in problem 5

--- intro-and-basics-syntax/exercise/05.CharacterStats.cs
+++ intro-and-basics-syntax/exercise/05.CharacterStats.cs
@@ -12,9 +12,9 @@ namespace CharacterStats05
         {
             // input data from console
             // for test
-            string name = Console.ReadLine();
-            int currentHealth = int.Parse(Console.ReadLine());
-            int maxHealth = int.Parse(Console.ReadLine());
+            string characterName = Console.ReadLine();
+            int currentCharacterHealth = int.Parse(Console.ReadLine());
+            int maxCharacterHealth = int.Parse(Console.ReadLine());
             int currentEnergy = int.Parse(Console.ReadLine());
             int maxEnergy = int.Parse(Console.ReadLine());
 
@@ -22,8 +22,8 @@ namespace CharacterStats05
             // write result on console
             // edit for test
             // new comment
-            Console.WriteLine($"Name: {name}");
-            Console.WriteLine("Health: |{0}{1}|", new string('|', currentHealth), new string('.', maxHealth - currentHealth));
+            Console.WriteLine($"Name: {characterName}");
+            Console.WriteLine("Health: |{0}{1}|", new string('|', currentCharacterHealth), new string('.', maxCharacterHealth - currentCharacterHealth));
             Console.WriteLine("Energy: |{0}{1}|", new string('|', currentEnergy), new string('.', maxEnergy - currentEnergy));
 
 
