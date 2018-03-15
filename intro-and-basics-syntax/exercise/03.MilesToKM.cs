e8b08b55ffc7d61027f75f09a8d867206f8c8de0
Mustafa Yakupov <mustafa.yakupov97@gmail.com>
2018-03-15 13:50:28 +0200

    .

---
+++ intro-and-basics-syntax/exercise/03.MilesToKM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MilesToKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double miles = double.Parse(Console.ReadLine());
+
+            double km = miles * 1.60934;
+
+            Console.WriteLine($"{km:f2}");
+        }
+    }
+}
