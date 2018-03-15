6812105a7701ff3df105fa755b535987037ad93d
AntoniaShalamanova <antonia.shalamanova@gmail.com>
2018-03-15 14:02:58 +0200

    document problem 3

--- intro-and-basics-syntax/exercise/04.BeverageLabels.cs
+++ intro-and-basics-syntax/exercise/04.BeverageLabels.cs
@@ -6,27 +6,25 @@ namespace _04.BeverageLabels
     {
         static void Main(string[] args)
         {
+            //We recieve from the Console the product name -> a string
             string name = Console.ReadLine();
+
+            /*In the next three lines, we recieve the volume,
+             * energy and sugar content of our product,
+             * as an integer
+             */
             int volume = int.Parse(Console.ReadLine());
             double energyPerHundredMl = double.Parse(Console.ReadLine());
             double sugarPerHundredMl = double.Parse(Console.ReadLine());
-
+            
+            //We calculate
             double energyPerDrink = (energyPerHundredMl * volume)/100;
             double sugarPerDrink = (sugarPerHundredMl * volume)/100;
 
+            //TODO
             Console.WriteLine("{0}ml {1}:", volume, name);
             Console.WriteLine("{0}kcal, {1}g sugars", energyPerDrink, sugarPerDrink);
-
-            //Console.WriteLine($"{volume}ml {name}:\r\n{energyPerDrink}kcal, {sugarPerDrink}g sugars");
-
         }
     }
 }
 
-/*
-Nuke-Cola
-220
-300
-70
-
-*/
--- intro-and-basics-syntax/exercise/05.CharacterStats.cs
+++ intro-and-basics-syntax/exercise/05.CharacterStats.cs
@@ -10,12 +10,15 @@ namespace CharacterStats05
     {
         static void Main(string[] args)
         {
+            // input data from console
             string name = Console.ReadLine();
             int currentHealth = int.Parse(Console.ReadLine());
             int maxHealth = int.Parse(Console.ReadLine());
             int currentEnergy = int.Parse(Console.ReadLine());
             int maxEnergy = int.Parse(Console.ReadLine());
 
+
+            // write result on console
             Console.WriteLine($"Name: {name}");
             Console.WriteLine("Health: |{0}{1}|", new string('|', currentHealth), new string('.', maxHealth - currentHealth));
             Console.WriteLine("Energy: |{0}{1}|", new string('|', currentEnergy), new string('.', maxEnergy - currentEnergy));
