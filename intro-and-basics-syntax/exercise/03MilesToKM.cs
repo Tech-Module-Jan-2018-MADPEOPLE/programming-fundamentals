8b587b40f2a8d681c8b9f2f4c8820301b1c757a7
AntoniaShalamanova <antonia.shalamanova@gmail.com>
2018-03-15 13:59:17 +0200

    Document problem 3

--- intro-and-basics-syntax/exercise/03MilesToKM.cs
+++ intro-and-basics-syntax/exercise/03MilesToKM.cs
@@ -6,10 +6,13 @@ namespace MilesToKm
     {
         static void Main(string[] args)
         {
+            // parse the miles to double number
             double miles = double.Parse(Console.ReadLine());
 
+            //convert miles to kilometers
             double km = miles * 1.60934;
 
+            //round to the second digit after the decimal point
             Console.WriteLine($"{km:f2}");
         }
     }
