d331235a6aad238e7c9dc336db9eee6736682e09
Mustafa Yakupov <mustafa.yakupov97@gmail.com>
2018-03-15 13:54:32 +0200

    Add comments

--- intro-and-basics-syntax/exercise/01.DebitCard.cs
+++ intro-and-basics-syntax/exercise/01.DebitCard.cs
@@ -6,11 +6,13 @@ namespace DebitCardNum
     {
         static void Main(string[] args)
         {
+		//We receive from the Console four numbers
             int num1 = int.Parse(Console.ReadLine());
             int num2 = int.Parse(Console.ReadLine());
             int num3 = int.Parse(Console.ReadLine());
             int num4 = int.Parse(Console.ReadLine());
-
+		
+		//TODO
             Console.WriteLine($"{num1:d4} {num2:d4} {num3:d4} {num4:d4}");
         }
     }
